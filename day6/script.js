5c9e56589a25c22b5e1f3bb9700fa9a1d829e500
jnanavi <jnanavidg2006@gmail.com>
2026-08-19 11:07:09 +0530

    new commit

---
+++ day6/script.js
@@ -0,0 +1,43 @@
+// function add(a , b){
+//     return a + b
+// }
+// console.log(add(10, 20))
+
+// console.log("Hello World")
+
+// let students = "john"
+// let age = 20
+// let isstudent = true
+// let salary = null
+// let address = undefined
+
+// console.log(typeof students)
+// console.log(typeof age)
+// console.log(typeof isstudent)
+// console.log(typeof salary)
+// console.log(typeof address)
+
+// let a = 10
+// let b = 20
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// let age = 20
+// if(age >= 18){
+//     console.log("You are eligible to vote")
+// } else {
+//     console.log("You are not eligible to vote")
+// }
+
+let age = 20
+
+console.log(age > 18)
+console.log(age < 18)
+console.log(age >= 18)
+console.log(age <= 18)
+console.log(age == 18)
+console.log(age != 18)
